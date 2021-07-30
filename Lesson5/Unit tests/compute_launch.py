f582ecda3de4a223e33f7e4928fbd8bd7a136080
Zacks <Zacks.Shen@gmail.com>
2021-07-29 23:21:46 -0700

    Update

--- Lesson5/Unit tests/compute_launch.py
+++ Lesson5/Unit tests/compute_launch.py
@@ -1,6 +1,6 @@
 def days_until_launch(current_day, launch_day):
     """"Returns the days left before launch.
-    
+
     current_day (int) - current day in integer
     launch_day (int) - launch day in integer
     """
